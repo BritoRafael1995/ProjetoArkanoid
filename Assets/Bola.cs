b24c57b25f1c52d91e4ff89d0d8fe973ba1c297d
Rafael Brito <britorafael1995@gmail.com>
2019-09-30 23:12:03 -0300

    primeiro commit, arquivos jogo

---
+++ Assets/Bola.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    public Vector2 velocidade;
+    void Start()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(velocidade);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
